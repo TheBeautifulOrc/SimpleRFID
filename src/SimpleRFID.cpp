fe216a3c2100d04e3fd420d6414d4116f42898d4
Luai Malek <luai_malek@gmx.de>
2019-04-04 21:25:34 +0200

    Finished prototype source-code

--- src/SimpleRFID.cpp
+++ src/SimpleRFID.cpp
@@ -2,7 +2,29 @@
 #include "SoftwareSerial.h"
 #include "SimpleRFID.h"
 
-SIMPLE_RFID::SIMPLE_RFID(int tx_pin, int rx_pin)
+SimpleRFID::SimpleRFID(int tx_pin, int rx_pin)
 {
-    _simpleRFID_SoftSerial = SoftwareSerial(tx_pin, rx_pin);#
+    _simpleRFID_SoftSerial = SoftwareSerial(tx_pin, rx_pin);
+    _simpleRFID_SoftSerial.begin(9600); //The RFID readers baudrate is fixed at 9600
+}
+
+SimpleRFID::available()
+{
+    return _simpleRFID_SoftSerial.available();
+}
+
+SimpleRFID::readID()
+{
+    String result = "";
+    unsigned char temp_char;
+    int check_int;
+    do{
+        temp_char = _simpleRFID_SoftSerial.read();
+        check_int = (int) temp_char;
+        if(check_int != 2 && check_int != 3 && check_int != 255)
+        {
+            result += (String) temp_char;
+        }
+    }while(check_int != 3);
+    return result;
 }
