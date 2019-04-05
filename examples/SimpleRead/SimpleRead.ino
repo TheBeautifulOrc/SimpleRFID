54c5d7625f5dea87b32d13c3e900cc91ce808a62
anfark <roman-schulte@gmx.de>
2019-04-05 08:19:09 +0200

    refactor examples

--- examples/SimpleRead/SimpleRead.ino
+++ examples/SimpleRead/SimpleRead.ino
@@ -1,16 +1,16 @@
 /*
+  Prints out the ID of any chip you scan.
+  
   Created on 4 Apr 2019 by
   Luai Malek and
   Roman Schulte
 */
 
-/*
-  Prints out the ID of any chip you scan.
-*/
-
 #include "SimpleRFID.h"
 
 String ID = "";
+
+// Setup RFID reader on pin 2 and 3 (D2 on base shield).
 const uint8_t rfid_rx_pin = 2;
 const uint8_t rfid_tx_pin = 3;
 SimpleRFID simple_rfid(rfid_rx_pin, rfid_tx_pin);
